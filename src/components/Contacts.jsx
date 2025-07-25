6df029dfaaba4fc1132385165ccae6ca388d9c09
Domy15 <domenico2016dellaventura@gmail.com>
2025-07-25 18:45:00 +0200

    sistemazioni

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -15,37 +15,39 @@ const Contacts = () => {
                     </Col>
                     <Col xs={6}>
                         <p className="fs-5 mt-4" style={{ color: "#D1D5D0" }}>
-                            Sono un giovane
-                            professionista con una forte
-                            vocazione verso l'architettura, che
-                            mi spinge ad impegnarmi con tutto
-                            me stesso in questo lavoro, con
-                            l'obiettivo di raggiungere numerosi
-                            traguardi per sviluppare le mie
-                            competenze e metterle a servizio
-                            della comunità.
-                            Mi occupo di varie attività: dalla
-                            progettazione architettonica alle
-                            sanatorie edilizie; fornisco
-                            supporto ai clienti che sono
-                            intenzionati ad acquistare un
-                            immobile, curando gli aspetti
-                            tecnici.
-                            Ho maturato un’esperienza
-                            considerevole nel settore, e la
-                            mia giovane età non può che
-                            determinarmi ad accogliere sempre
-                            nuovi stimoli.
+                            <em>
+                                Sono un giovane
+                                professionista con una forte
+                                vocazione verso l'architettura, che
+                                mi spinge ad impegnarmi con tutto
+                                me stesso in questo lavoro, con
+                                l'obiettivo di raggiungere numerosi
+                                traguardi per sviluppare le mie
+                                competenze e metterle a servizio
+                                della comunità.
+                                Mi occupo di varie attività: dalla
+                                progettazione architettonica alle
+                                sanatorie edilizie; fornire
+                                supporto ai clienti che sono
+                                intenzionati ad acquistare un
+                                immobile, curando gli aspetti
+                                tecnici.
+                                Ho maturato un’esperienza
+                                considerevole nel settore, e la
+                                mia giovane età non può che
+                                determinarmi ad accogliere sempre
+                                nuovi stimoli.
+                            </em>
                         </p>
                     </Col>
                 </Row>
                 <div className="text-center w-50 m-auto">
-                    <p className="mb-3 fw-semibold fs-5" style={{color: "#9CA3AF"}}>Arch. Gennaro Lauri</p>
-                    <p className="mb-2">📧 <a className="footer-link text-decoration-none" style={{color: "#9CA3AF"}}>Email: arch.gennarolauri@gmail.com</a></p>
-                    <p className="mb-2">✉️ <a className="footer-link text-decoration-none" style={{color: "#9CA3AF"}}>Pec: gennaro.lauri@archiworldpec.it</a></p>
-                    <p className="mb-2"><Instagram className="text-white me-2" /><a href="https://www.instagram.com/gennaro_lauri_architetetto/" className="footer-link" style={{color: "#9CA3AF"}}>gennaro_lauri_architetetto</a></p>
-                    <p className="mb-2">📱 <a className="footer-link text-decoration-none" style={{color: "#9CA3AF"}}>+39 366 30 53 166</a></p>
-                    <p style={{color: "#9CA3AF"}}>📍 Padova</p>
+                    <p className="mb-3 fw-semibold fs-5" style={{ color: "#9CA3AF" }}>Arch. Gennaro Lauri</p>
+                    <p className="mb-2">📧 <a className="footer-link text-decoration-none" style={{ color: "#9CA3AF" }}>Email: arch.gennarolauri@gmail.com</a></p>
+                    <p className="mb-2">✉️ <a className="footer-link text-decoration-none" style={{ color: "#9CA3AF" }}>Pec: gennaro.lauri@archiworldpec.it</a></p>
+                    <p className="mb-2"><Instagram className="text-white me-2" /><a href="https://www.instagram.com/gennaro_lauri_architetto/" className="footer-link" style={{ color: "#9CA3AF" }}>gennaro_lauri_architetto</a></p>
+                    <p className="mb-2">📱 <a className="footer-link text-decoration-none" style={{ color: "#9CA3AF" }}>+39 366 30 53 166</a></p>
+                    <p style={{ color: "#9CA3AF" }}>📍 Padova</p>
                 </div>
             </div>
         </div>
--- src/components/Philosophy.jsx
+++ src/components/Philosophy.jsx
@@ -1,8 +1,8 @@
 const Philosophy = () => {
     return (
-        <div className="text-center mx-auto padding-standard" id="filosofia" style={{ width: "40%" }}>
+        <div className="text-center mx-auto padding-standard text-white" id="filosofia" style={{ width: "40%" }}>
             <h2 className="font-libre fs-1">La Nostra Filosofia</h2>
-            <p className="fs-5 mt-4 font-gray">
+            <p className="fs-5 mt-4">
                 Nel mondo dell'architettura, non si tratta solo di costruire edifici, ma di creare ambienti che
                 ispirano, funzionano e durano nel tempo. Sono qui per accompagnarti in questo viaggio,
                 trasformando le tue idee più ambiziose in realtà tangibili.
