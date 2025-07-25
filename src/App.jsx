e7c38a758e575f6e465882177afd8c620c6c745c
Domy15 <domenico2016dellaventura@gmail.com>
2025-07-25 18:06:24 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/Navbar'
+import NavBar from './components/NavBar'
 import Hero from './components/Hero';
 import Philosophy from './components/Philosophy';
 import Skills from './components/Skills';
