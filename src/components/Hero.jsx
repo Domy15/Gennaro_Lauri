0cda227317cc374cbcc25f4113178a5cbb380bdd
Domy15 <domenico2016dellaventura@gmail.com>
2025-07-26 12:07:42 +0200

    aggiustamenti responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
     return (
 
         <div className="padding-custom w-50 mx-auto text-center" id="#" >
-            <h1 className="fw-bolder" style={{ fontSize: "4.5em" }}>Trasforma i Tuoi Sogni in Spazi Concreti.</h1>
+            <h1 className="fw-bolder main-title" style={{ fontSize: "4.5em" }}>Trasforma i Tuoi Sogni in Spazi Concreti.</h1>
             <p className="fs-3 fw-light font-gray">Il Tuo Spazio. La Tua Visione. La Nostra Expertise.</p>
         </div>
     );
